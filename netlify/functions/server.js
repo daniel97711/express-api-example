a3b5d7af6032445c7255ae17084c531dd76d66a2
Daniel <daniellai9771@gmail.com>
2024-03-20 02:31:23 +1100

    s

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -12,7 +12,7 @@ const app = express();
 const router = Router();
 const config = require('../../src/config.json');
 const {generateHtml, isTokenValid} = require('../../src/generateHTML.js');
-// const {userRegister, changePassword} = require('../../src/user.js');
+const {userRegister, changePassword} = require('../../src/user.js');
 const {clear} = require('../../src/data.js');
 const {userLogin, userLogout} = require('../../src/userLogin.js');
 
