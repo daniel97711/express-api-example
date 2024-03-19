3a82b34320517af7f9bfb6a89d6e490b1ce3e73a
Daniel <daniellai9771@gmail.com>
2024-03-20 02:54:57 +1100

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -27,6 +27,77 @@ router.get("/", (req, res) => {
   res.send({ data: "Hello SENG2021!" });
 });
 
+// user register
+router.put('/user/register', (req, res) => {
+  const email = req.body.email;
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  try {
+    const value = userRegister(email, password, userName);
+    res.status(200).json(value);
+  } catch (error) {
+    res.status(error.statusCode).json({error: error.message});
+  }
+});
+
+// change password
+router.post('/user/password', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const newPassword = req.body.newPassword;
+
+  try {
+    const value = changePassword(email, password, newPassword);
+    res.status(200).json(value);
+  } catch (error) {
+    res.status(error.statusCode).json({error: error.message});
+  }
+});
+
+// help the user to login to the system
+router.post('/user/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const value = userLogin(email, password);
+    res.status(200).json(value);
+  } catch (error) {
+    res.status(error.statusCode).json({error: error.message});
+  }
+});
+
+// help the user to logout from the system
+router.post('/user/logout', (req, res) => {
+  const userId = req.body.userId;
+  const token = req.body.token;
+
+  try {
+    const value = userLogout(userId, token);
+    res.status(200).json(value);
+  } catch (error) {
+    res.status(error.statusCode).json({error: error.message});
+  }
+});
+
+// clear all the data from data.json
+router.delete('/clear', (req, res) => {
+  const value = clear();
+  return res.json(value);
+});
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({storage: storage}).single('xmlFile');
+
+
+
+
 router.get("/slow", (req, res) => {
   const { delay } = req.query;
 
